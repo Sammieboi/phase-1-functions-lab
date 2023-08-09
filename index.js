089a197cfdc98c5d8b4ea8886636bfe0e5eba12e
samuel dufresne <sdufr@yahoo.com>
2023-08-09 09:26:28 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+
+
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet(blocks) {
+
+    const blocksToFeetConversion = 264;
+    return distanceFromHqInBlocks(blocks) * blocksToFeetConversion;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+
+    const blocksToFeetConversion = 264;
+    return Math.abs(endBlock - startBlock) * blocksToFeetConversion;
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+
+    const feetTravelled = distanceTravelledInFeet(startBlock, endBlock);
+
+    if (feetTravelled <= 400) {
+        return 0;
+    } else if (feetTravelled <= 2000) {
+        return (feetTravelled - 400) * 0.02;
+    } else if (feetTravelled <= 2500) {
+        return 25;
+    } else {
+        return 'does not allow rides over 2500 feet', 'cannot travel that far'
+    }
+}
+
+module.exports = {
+    distanceFromHqInBlocks,
+    distanceFromHqInFeet,
+    distanceTravelledInFeet,
+    calculatesFarePrice
+};
